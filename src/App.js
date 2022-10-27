e75c5681147460d2c7ebcfbe3a50aeb7db64b634
Iniomoh Angela <angela.iniomoh@gmail.com>
2022-10-28 00:30:43 +0100

    src/Components/Input.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "./Components/Button";
 
 function App() {
   return (
     <div className="App">
-        <div className="calc-wrapper">
-         <h1>Heading</h1>
+      <div className="calc-wrapper">
+        <div className="row">
+          <Button symbol="7" />
+          <Button symbol="8" />
+          <Button symbol="9" />
+          <Button symbol="/" />
         </div>
+        <div className="row">
+          <Button symbol="4" />
+          <Button symbol="5" />
+          <Button symbol="6" />
+          <Button symbol="*" />
+        </div>
+        <div className="row">
+          <Button symbol="1" />
+          <Button symbol="2" />
+          <Button symbol="3" />
+          <Button symbol="+" />
+        </div>
+        <div className="row">
+          <Button symbol="0" />
+          <Button symbol="." />
+          <Button symbol="=" />
+          <Button symbol="-" />
+        </div>
+      </div>
     </div>
   );
 }
