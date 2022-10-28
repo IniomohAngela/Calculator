ec03e883443aba16fb7e4c0bbd3423b474eeb113
Iniomoh Angela <angela.iniomoh@gmail.com>
2022-10-28 01:09:38 +0100

    modified:   src

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Button from "./Components/Button";
 import Input from "./Components/Input";
 
 function App() {
+
+  const buttonColor = "#f2a33c";
   return (
     <div className="App">
       <div className="calc-wrapper">
@@ -11,27 +13,27 @@ function App() {
           <Button symbol="7" />
           <Button symbol="8" />
           <Button symbol="9" />
-          <Button symbol="/" />
+          <Button symbol="/" color={buttonColor} />
         </div>
         <div className="row">
           <Button symbol="4" />
           <Button symbol="5" />
           <Button symbol="6" />
-          <Button symbol="*" />
+          <Button symbol="*" color={buttonColor} />
         </div>
         <div className="row">
           <Button symbol="1" />
           <Button symbol="2" />
           <Button symbol="3" />
-          <Button symbol="+" />
+          <Button symbol="+" color={buttonColor} />
         </div>
         <div className="row">
           <Button symbol="0" />
           <Button symbol="." />
           <Button symbol="=" />
-          <Button symbol="-" />
+          <Button symbol="-" color={buttonColor} />
         </div>
-        <Button symbol="Clear" />
+        <Button symbol="Clear" color="red" />
       </div>
     </div>
   );
