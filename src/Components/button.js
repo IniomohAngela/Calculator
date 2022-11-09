84b1b136eb8d4226a821a7c3919279de382ee975
Iniomoh Angela <angela.iniomoh@gmail.com>
2022-11-09 16:35:32 +0000

    Update button.js

--- src/Components/button.js
+++ src/Components/button.js
@@ -1,15 +1,15 @@
 import "./Button.css";
 
 const Button = ({ symbol, color, handleClick }) => {
-    return(
-     <div 
-     onClick={() => handleClick(symbol)}
-     className="button-wrapper">{symbol}</div>
-     style={{ backgroundColor: color }}
+  return (
+    <div
+      onClick={() => handleClick(symbol)}
+      className="button-wrapper"
+      style={{ backgroundColor: color }}
     >
       {symbol}
     </div>
-);
+  );
 };
 
 export default Button;
